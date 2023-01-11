a383c621ef3f5057c3cf3bee076aeb508e8daff7
DanielMalMed <Danielmalmed@outlook.com>
2023-01-10 16:07:32 -0800

    New places can be added

---
+++ models/places.js
@@ -0,0 +1,15 @@
+
+  // GET /places
+  module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/thaifood.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/codingcat.jpg'
+  }]
